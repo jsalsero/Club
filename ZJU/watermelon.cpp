fffa261f3c578e7846d31c15ae3cb5890d5795fe
Jimmyzaurio <wizard.jsp@gmail.com>
2016-04-25 17:19:30 -0500

    taaaaabs

--- ZJU/watermelon.cpp
+++ ZJU/watermelon.cpp
@@ -13,39 +13,39 @@ struct SegTree {
 	SegTree(int I, int D) 
 		: izq(NULL), der(NULL),
 		  i(I), d(D), dato() {}
-    
-    ~SegTree() {
-    	if (izq) delete izq;
-    	if (der) delete der;
-    }
+	
+	~SegTree() {
+		if (izq) delete izq;
+		if (der) delete der;
+	}
 
-    T Construir() {
-        if (i == d) return dato = T();
-        int m = (i + d) >> 1;
-        izq = new SegTree(i, m);
-        der = new SegTree(m + 1, d);
-        return dato = izq->Construir() +
-                      der->Construir();
-    }
+	T Construir() {
+		if (i == d) return dato = T();
+		int m = (i + d) >> 1;
+		izq = new SegTree(i, m);
+		der = new SegTree(m + 1, d);
+		return dato = izq->Construir() +
+					  der->Construir();
+	}
 
-    T Actualizar(int a, T v) {
-    	if (a < i || d < a) return dato;
-    	if (a == i && d == a) return dato = min(dato, v);
-    	if (!izq) {
-    		int m = (i + d) >> 1;
-    		izq = new SegTree(i, m);
-    		der = new SegTree(m + 1, d);
-    	}
-    	return dato = min(izq->Actualizar(a, v),
-    				  der->Actualizar(a, v));
-    }
+	T Actualizar(int a, T v) {
+		if (a < i || d < a) return dato;
+		if (a == i && d == a) return dato = min(dato, v);
+		if (!izq) {
+			int m = (i + d) >> 1;
+			izq = new SegTree(i, m);
+			der = new SegTree(m + 1, d);
+		}
+		return dato = min(izq->Actualizar(a, v),
+					  der->Actualizar(a, v));
+	}
 
-    T Query(int a, int b) {
-    	if (b < i || d < a) return T();
-    	if (a <= i && d <= b) return dato;
-    	return izq? min(izq->Query(a, b),
-    				der->Query(a, b)): T();
-    }
+	T Query(int a, int b) {
+		if (b < i || d < a) return T();
+		if (a <= i && d <= b) return dato;
+		return izq? min(izq->Query(a, b),
+					der->Query(a, b)): T();
+	}
 };
 
 struct Melon {
@@ -63,8 +63,8 @@ struct Melon {
 int main() {
 	Long N, num;
 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
 
 	while (cin >> N) {
 		vector<Melon> V;
@@ -81,12 +81,12 @@ int main() {
 
 		SegTree<Melon> tree(0, N - 1);
 
-        tree.Actualizar(min(tiempo[0] - 1, N - 1), V[0]);
+		tree.Actualizar(min(tiempo[0] - 1, N - 1), V[0]);
 		for (int i = 1; i < N; ++i) {
 			Melon aux = tree.Query(i - 1, N - 1);
-            tree.Actualizar(min(i + tiempo[i] - 1, N - 1), aux + V[i]);
+			tree.Actualizar(min(i + tiempo[i] - 1, N - 1), aux + V[i]);
 		}
-        cout << tree.Query(N - 1, N - 1).precio << "\n";
+		cout << tree.Query(N - 1, N - 1).precio << "\n";
 	}
 	return 0;
 }
